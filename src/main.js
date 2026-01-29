0b627c021259038931401b7a0ca6597099cc1b08
davi <davifilho.br@gmail.com>
2026-01-29 17:09:35 -0300

    feat: add new rules in password verification

--- src/main.js
+++ src/main.js
@@ -30,10 +30,9 @@ const init = () => {
   };
 
   const checkPasswordHaveLetterInUpperCase = (password) => {
-    const ruleThreeStatus =
-      document.getElementById("rule-4").children[0].children[0];
-    ruleThreeStatus.textContent = password.match(/[A-Z]/) ? "✅" : "❌";
-  };
+    const ruleFourStatus = document.getElementById("rule-4").children[0].children[0];
+    ruleFourStatus.textContent = password.match(/[A-Z]/) ? "✅" : "❌";
+  }
 
   const checkTeacherName = (password) => {
     const names = [
@@ -76,6 +75,21 @@ const init = () => {
     ruleSevenStatus.textContent = containsLogoName ? "✅" : "❌";
   };
 
+  const checkPasswordIsEmail = (password) => {
+    const ruleEitghStatus = document.getElementById("rule-8").children[0].children[0];
+    ruleEitghStatus.textContent = password.match(/.+@.+\.com$/) ? "✅" : "❌";
+  }
+
+  const checkPasswordHaveRomanNumeral = (password) => {
+    const ruleNineStatus = document.getElementById("rule-9").children[0].children[0];
+    ruleNineStatus.textContent = password.match(/^(?=.+$)M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/) ? "✅" : "❌";
+  }
+
+  const checkPasswordHaveLeapYear = (password) => {
+    const ruleTenStatus = document.getElementById("rule-10").children[0].children[0];
+    ruleTenStatus.textContent = password.match(/^(?:(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26]))|(?:0[48]|[2468][048]|[13579][26])00)$/) ? "✅" : "❌";
+  }
+
   passwordInput.addEventListener("input", (e) => {
     checkPasswordLength(e.target.value);
     checkPasswordHaveNumber(e.target.value);
@@ -84,7 +98,12 @@ const init = () => {
     checkTeacherName(e.target.value);
     checkSumToRandom(e.target.value);
     checkLogosNames(e.target.value);
+    checkPasswordIsEmail(e.target.value);
+    checkPasswordHaveRomanNumeral(e.target.value);
+    checkPasswordHaveLeapYear(e.target.value);
   });
 };
+
+
 
 init();
